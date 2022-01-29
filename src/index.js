b59e389610ea07c1ddd0532d6e91981e6b20e580
Ted Martin <theodorejmartin@outlook.com>
2022-01-28 22:14:56 -0600

    it works now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM  from "react";
+import ReactDOM  from "react-dom";
 import AppRouter from "./router/AppRouter";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
 
-ReactDOM.rendor(<AppRouter />, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
